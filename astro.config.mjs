baa79c420e129ae6a8dcdfb20c128632c75c8d58
simon <nong@none.com>
2023-04-27 10:53:22 +0900

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), svelte(), mdx(), vue()],
+  // output: 'server'
+});
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div className="flex h-full border-r border-slate-200 w-[60px] md:w-[180px] transition-all">
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;
